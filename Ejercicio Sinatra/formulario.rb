5c09b77c81164d7c0c77fcab7e3aa8a3d16110f5
Juan <juanbedon26@hotmail.com>
2018-03-16 14:42:06 -0500

    Ejercicio Formulario en Sinatra

---
+++ Ejercicio Sinatra/formulario.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+
+enable :sessions
+
+get "/" do
+	#puts session.inspect
+	if session["user"] && session["email"]
+		@user = session["user"]
+		@email = session["email"]
+
+		erb :post
+	else
+      @closed_session = session["closed_session"]
+      
+      erb :index
+  end
+end
+
+post "/login" do
+  @user = params["user"]
+  @email = params["email"]
+  session.store("user", @user)
+  session.store("email", @email)
+
+  erb :post
+end
+
+get "/log_out" do
+  session.delete("user")
+  session.delete("email")
+  session.store("closed_session", true)
+  redirect "/"
+end
